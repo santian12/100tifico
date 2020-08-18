09bfa89a20a35df18eda919120dfe4f82bac780e
Santiago Muñoz <santiagoanmucha@gmail.com>
2020-08-17 19:25:14 -0500

    agregue cambios a los estilos y la api funciona bien

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,8 @@
 const Error404 = () => {
     const view = `
       <div class="Error404">
-      <img src="https://image.shutterstock.com/image-vector/error-404-page-not-found-600w-1417730213.jpg "/>
+        
+      <img src="https://image.shutterstock.com/image-vector/error-404-page-not-found-600w-1417730213.jpg " />
         <h2>Error 404</h2>
       </div>
     `;
--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,17 +2,23 @@ const Header = () => {
     const view = `
       <div class="Header-main">
         <div class="Header-logo">
+          
           <h1>
             <a href="/">
-              100tifi.co
+              <img src="https://i.imgur.com/hBgVxQy.png" >
+           
             </a>
+            
           </h1>
+         
         </div>
+   
         <div class="Header-nav">
           <a href="#/about/">
             About
           </a>
         </div>
+        
       </div>
     `;
     return view;
